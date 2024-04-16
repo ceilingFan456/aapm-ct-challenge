82819db010643700f895a445e36d79b2b7dee670
Qiming <e0271228@u.nus.edu>
2024-04-16 19:42:35 +0200

    setup

--- aapm-ct/config.py
+++ aapm-ct/config.py
@@ -1,5 +1,9 @@
 import os
 
 
-DATA_PATH = os.path.join("raw_data")
-RESULTS_PATH = os.path.join("results")
+# DATA_PATH = os.path.join("raw_data")
+# RESULTS_PATH = os.path.join("results")
+
+
+DATA_PATH = os.path.join("raw_data_gz")
+RESULTS_PATH = os.path.join("results_real")
