03b2fdb7c8f1cab9fa0976281b4a92a765194c85
Qiming <e0271228@u.nus.edu>
2024-05-09 22:39:05 +0200

    Working with new dataset still debug

--- aapm-ct/check_dim.py
+++ aapm-ct/check_dim.py
@@ -2,24 +2,39 @@ import h5py
 import numpy as np 
 import os
 import gzip
+import matplotlib.pyplot as plt
+
 
 # Open the HDF5 file
 # file = h5py.File('./real_data_hdf5/ground_truth_test/ground_truth_test_000.hdf5', 'r')
 # file = h5py.File('./real_data_hdf5/observation_test/observation_test_027.hdf5', 'r')
-file = h5py.File('./real_data_hdf5/observation_test/observation_test_027.hdf5', 'r')
+# file = h5py.File('./real_data_hdf5/observation_test/observation_test_027.hdf5', 'r')
 
-data = file["data"]
+# file = h5py.File('./real_data_hdf5/observation_test/observation_test_027.hdf5', 'r')
 
-print(data.shape)
+# data = file["data"]
 
-file.close()
+# print(data.shape)
+
+# file.close()
 
 
 sinogram = np.load(
     gzip.GzipFile(
-        os.path.join("./data/", "Sinogram_batch{}.npy.gz".format(1)),
+        os.path.join("./real_data_modified/training_data", "Phantom_batch{}.npy.gz".format(1)),
         "r",
     )
 )
 
 print(sinogram.shape)
+
+new = sinogram[0, :, :]
+
+plt.figure(figsize=(10, 5))
+plt.imshow(new, cmap='gray', aspect='auto')
+plt.colorbar(label="Intensity")
+plt.title(f"Sinogram {0}")
+plt.xlabel("Detector Channels")
+plt.ylabel("Projections")
+plt.show()
+
