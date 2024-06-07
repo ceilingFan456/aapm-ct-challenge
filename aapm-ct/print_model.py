7a8631115ed8fc3e4fae62c869c8ccb2e36181a9
Qiming <e0271228@u.nus.edu>
2024-06-07 08:18:03 +0200

    Change plotting path

--- aapm-ct/print_model.py
+++ aapm-ct/print_model.py
@@ -6,7 +6,8 @@ from config import RESULTS_PATH
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load the model
-model_path = os.path.join(RESULTS_PATH, "operator_radon_fwd_train_phase_0", "model_weights_final.pt")
+# model_path = os.path.join(RESULTS_PATH, "operator_radon_fwd_train_phase_0", "model_weights_final.pt")
+model_path = os.path.join(RESULTS_PATH, "operator_radon_fwd_train_phase_29", "model_weights.pt")
 model = torch.load(model_path, map_location=device)
 
 # Print all parameters of the model
