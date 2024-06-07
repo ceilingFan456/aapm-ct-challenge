b9f38d977277b6bc8c5d8b3107d45298a6573494
Qiming <e0271228@u.nus.edu>
2024-06-07 08:32:37 +0200

    Fix

--- aapm-ct/print_model.py
+++ aapm-ct/print_model.py
@@ -7,7 +7,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load the model
 # model_path = os.path.join(RESULTS_PATH, "operator_radon_fwd_train_phase_0", "model_weights_final.pt")
-model_path = os.path.join(RESULTS_PATH, "operator_radon_fwd_train_phase_29", "model_weights.pt")
+model_path = os.path.join(RESULTS_PATH, "operator_radon_fwd_train_phase_2", "model_weights.pt")
 model = torch.load(model_path, map_location=device)
 
 # Print all parameters of the model
