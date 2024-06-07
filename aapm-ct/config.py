23e82968548c89ab8b6603a56e9a636c0c9f1f08
Qiming <e0271228@u.nus.edu>
2024-06-08 00:24:57 +0200

    Fix

--- aapm-ct/config.py
+++ aapm-ct/config.py
@@ -1,8 +1,10 @@
 import os
 
+DATA_PATH = os.path.join("raw_data")
+RESULTS_PATH = os.path.join("results_original")
 
-DATA_PATH = os.path.join("real_data_modified")
-RESULTS_PATH = os.path.join("results_new")
+# DATA_PATH = os.path.join("real_data_modified")
+# RESULTS_PATH = os.path.join("results_new")
 
 
 # DATA_PATH = os.path.join("real_data_gz")
