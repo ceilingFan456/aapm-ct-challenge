f7da047cd66a9a6096b7c553bcd68b834ec51278
Qiming <e0271228@u.nus.edu>
2024-06-09 00:55:54 +0200

    fix

--- aapm-ct/script_radon_identify.sh
+++ aapm-ct/script_radon_identify.sh
@@ -6,19 +6,28 @@
 #PBS -l walltime=01:00:00
 #PBS -N forward 
 
-# Load necessary modules
-module load cudatoolkit/10.1
-module load python/3.8.3
-module load pytorch/1.6.0
-module load torchvision/0.7.0
-module load matplotlib/3.1.3
-module load numpy/1.18.1
-module load pandas/1.0.5
-module load tqdm/4.46.0
+# Load available modules
+module load python/3.8.13
+module load cuda/11.6.2
+module load pytorch/1.11.0-py3-gpu
 
-# Change to the directory from which the job was submitted
 cd ${PBS_O_WORKDIR}
+
+# Create a virtual environment
+python -m venv myenv
+
+# Activate the virtual environment
+source myenv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install required packages
+pip install matplotlib numpy pandas torchvision tqdm
 
 # Run the Python script
 python config.py
 python script_radon_identify.py
+
+# Deactivate the virtual environment
+deactivate
