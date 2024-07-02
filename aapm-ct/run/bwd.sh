2ed911a97109b92bc68ee1bcda7945c73372328a
Huang Qiming <e0271228@asp2a-login-nus02.head.cm.asp2a.nscc.sg>
2024-07-02 08:38:29 +0800

    Script can run on cloud

--- aapm-ct/run/bwd.sh
+++ aapm-ct/run/bwd.sh
@@ -12,10 +12,7 @@ echo "Job started at: $(date)"
 module load miniforge3
 codna activate myenv
 
-python -c "import numpy; print('NumPy version:', numpy.__version__)"
-python -c "import matplotlib; print('Matplotlib version:', matplotlib.__version__)"
-
 cd /home/users/nus/e0271228/aapm-ct-challenge/aapm-ct/
 
 python config.py
-python script_radon_identify.py
+python script_radon_learn_inv.py
