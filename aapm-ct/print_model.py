af3e8c04caf62a0732223079e0bbf66c343cf876
Huang Qiming <e0271228@asp2a-login-nus01.head.cm.asp2a.nscc.sg>
2024-07-13 21:59:41 +0800

    to merge

--- aapm-ct/print_model.py
+++ aapm-ct/print_model.py
@@ -7,9 +7,12 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Load the model
 # model_path = os.path.join(RESULTS_PATH, "operator_radon_fwd_train_phase_0", "model_weights_final.pt")
-model_path = os.path.join(RESULTS_PATH, "operator_radon_fwd_train_phase_2", "model_weights.pt")
+model_path = os.path.join(RESULTS_PATH, "operator_radon_fwd_train_phase_30", "model_weights.pt")
 model = torch.load(model_path, map_location=device)
 
 # Print all parameters of the model
 for name, param in model.items():
     print(name, param)
+    if name == "OpR.angles":
+        print(param[1:] - param[:-1])
+
